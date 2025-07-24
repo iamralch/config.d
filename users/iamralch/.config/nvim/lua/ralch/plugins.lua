6934fba4d127b187f9499b625412fee8e8bc8137
Svetlin Ralchev <svetlin@ralch.com>
2025-07-24 13:59:34 +0400

    feat(nvim): replace copilot-lua-cmp with copilot-cmp for completion
    Switched completion plugin from copilot-lua-cmp to copilot-cmp in
    plugins configuration and updated lazy-lock.json accordingly.
    
    Signed-off-by: Svetlin Ralchev <svetlin@ralch.com>

--- users/iamralch/.config/nvim/lua/ralch/plugins.lua
+++ users/iamralch/.config/nvim/lua/ralch/plugins.lua
@@ -3,8 +3,8 @@ return {
   "AstroNvim/astrocommunity",
 
   -- Completion
+  { import = "astrocommunity.completion.copilot-cmp" },
   { import = "astrocommunity.completion.blink-cmp-git" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   -- Editing Support
   { import = "astrocommunity.editing-support.codecompanion-nvim" },
