4539e0c23107bff2352d63e2922ce31c72e8c5a8
Svetlin Ralchev <svetlin@ralch.com>
2025-07-28 09:33:42 +0400

    feat(plugins): add Quarto support and update plugin lockfile
    - Added `astrocommunity.pack.quarto` to enable Quarto support.
    - Updated `lazy-lock.json` with new plugin versions and added entries for `otter.nvim` and `quarto-nvim`.
    
    Signed-off-by: Svetlin Ralchev <svetlin@ralch.com>

--- users/iamralch/.config/nvim/lua/ralch/plugins.lua
+++ users/iamralch/.config/nvim/lua/ralch/plugins.lua
@@ -46,6 +46,7 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.quarto" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
